e2292fac2d34aef5f9ebbd93946507bf9f7e6e1f
alex <dam>
2021-04-23 18:12:21 -0400

    connected to db

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -6,8 +6,35 @@ import http.server
 import requests
 import os
 from urllib.parse import unquote, parse_qs
-import threading
+import threading 
 from socketserver import ThreadingMixIn
+import psycopg2
+from config import config
+
+def connectDB(): 
+  """connect to postgres"""
+  connection = None
+  try:
+    params = config()
+
+    connection = psycopg2.connect(**params)
+
+    cursor = connection.cursor()
+
+    print('PostgreSQL database version:')
+    cursor.execute('SELECT version()')
+
+    db_version = cursor.fetchone()
+    print(db_version)
+
+    cursor.close()
+  except (Exception, psycopg2.DatabaseError) as error:
+    print(error)
+  finally:
+    if connection is not None:
+      connection.close()
+      print('db connection closed')
+
 
 memory = {}
 
@@ -125,6 +152,8 @@ class Shortener(http.server.BaseHTTPRequestHandler):
                 "Couldn't fetch URI '{}'. Sorry!".format(longuri).encode())
 
 if __name__ == '__main__':
+    connectDB()
     server_address = ('', int(os.environ.get('PORT', '8000')))
     httpd = ThreadHTTPServer(server_address, Shortener)
     httpd.serve_forever()
+    
